b032c683a055ddfe83f875b84838ba99f1a7a739
louajlassi <loua.jlassi@eprit.tn>
2022-03-24 19:39:11 +0100

    com3

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include<stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+
 #include "backg.h"
 
 int main(){
@@ -9,10 +12,14 @@ Background b ;
 SDL_Surface *screen ; 
 SDL_Surface *back ; 
 
+SDL_Init(SDL_INIT_EVERYTHING);
+
+screen=SDL_SetVideoMode (1000,600,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
 
 initBack(&b);
 
 back = IMG_Load("backg.jpg");
+SDL_BlitSurface(back,NULL,screen,NULL);
 afficherBack(b,screen);
 
 }
